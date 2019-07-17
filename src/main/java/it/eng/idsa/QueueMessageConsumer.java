b92e8bae0a6b9335572c0d0f198a2980635cec08
Milan Karajovic <milan.karajovic@eng.it>
2019-07-17 15:12:58 +0200

    Adapted for the Web.

--- src/main/java/it/eng/idsa/QueueMessageConsumer.java
+++ src/main/java/it/eng/idsa/QueueMessageConsumer.java
@@ -8,14 +8,26 @@ import javax.jms.MessageConsumer;
 import javax.jms.MessageListener;
 import javax.jms.Session;
 import javax.jms.TextMessage;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.log4j.Logger;
+import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
+import org.glassfish.jersey.client.ClientConfig;
+
+import it.eng.idsa.util.PropertiesConfig;
 
 /**
  * Message consumer which consumes the message from ActiveMQ Broker
  */
 public class QueueMessageConsumer implements MessageListener {
+	private static final PropertiesConfig CONFIG_PROPERTIES = PropertiesConfig.getInstance();
 	private static final String CONSUMER_MESSAGE_RECEIVED = "QueueMessageConsumer Received message [ %s ]";
 
 	private static Logger LOG = Logger.getLogger(QueueMessageConsumer.class.getName());
@@ -52,9 +64,23 @@ public class QueueMessageConsumer implements MessageListener {
                     ((TextMessage) message).getText());
             //Thread.sleep(1000);// sleep for 10 seconds
             System.out.println(msg);
+            senReceivedMessage(msg);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    public void senReceivedMessage(String msg) {
+    	ClientConfig config = new ClientConfig();
+		config.connectorProvider(new ApacheConnectorProvider());
+		Client client = ClientBuilder.newClient(config);
+		WebTarget webTarget = client.target(CONFIG_PROPERTIES.getProperty("webUri"));
+		 
+		Invocation.Builder invocationBuilder =  webTarget.request(MediaType.APPLICATION_JSON);
+		 
+		Response response = invocationBuilder.post(Entity.entity(msg, MediaType.APPLICATION_JSON));
+	
+		System.out.println("response="+response.readEntity(String.class));
     }
 
     public String getDestinationName() {
